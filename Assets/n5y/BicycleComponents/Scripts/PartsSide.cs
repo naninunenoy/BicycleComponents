23f688b4b661829e6493c5acce054e69bfed6bef
naninunenoy <naninuneno0411@gmail.com>
2020-09-24 00:54:19 +0900

    enum

---
+++ Assets/n5y/BicycleComponents/Scripts/PartsSide.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace n5y.BicycleComponents {
+    /// <summary>
+    /// 左右
+    /// </summary>
+    public enum PartsSide {
+        /// <summary> なし </summary>
+        None = 0,
+        /// <summary> 左 </summary>
+        Left,
+        /// <summary> 右 </summary>
+        Right,
+        /// <summary> 前 </summary>
+        Front,
+        /// <summary> 後 </summary>
+        Rear
+    }
+}
---
+++ Assets/n5y/BicycleComponents/Scripts/PartsType.cs
@@ -0,0 +1,24 @@
+﻿namespace n5y.BicycleComponents {
+    public enum PartsType {
+        /// <summary> フレーム </summary>
+        Frame = 0,
+        /// <summary> ハンドル </summary>
+        Handle,
+        /// <summary> サドル </summary>
+        Saddle,
+        /// <summary> 前輪 </summary>
+        FrontWheel,
+        /// <summary> 後輪 </summary>
+        RearWheel,
+        /// <summary> 左ペダル </summary>
+        LeftPedal,
+        /// <summary> 右ペダル </summary>
+        RightPedal,
+        /// <summary> 左ペダルクランク </summary>
+        LeftPedalCrankArm,
+        /// <summary> 右ペダルクランク </summary>
+        RightPedalCrankArm,
+        /// <summary> チェーン </summary>
+        Chain
+    }
+}
