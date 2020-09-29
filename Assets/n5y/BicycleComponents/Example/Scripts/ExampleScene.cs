a5f68cd71f1814a186af62434ddb22e43e283f30
naninunenoy <naninuneno0411@gmail.com>
2020-09-29 23:37:22 +0900

    UI作成

---
+++ Assets/n5y/BicycleComponents/Example/Scripts/ExampleScene.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace n5y.BicycleComponents.Example {
+    public class ExampleScene : MonoBehaviour {
+        [SerializeField] Slider speedSlider = default;
+        [SerializeField] Text speedText = default;
+        [SerializeField] Slider handleSlider = default;
+        [SerializeField] Text handleText = default;
+
+        void Start() {
+            handleSlider
+                .onValueChanged
+                .AddListener(value => {
+                    handleText.text = $"{value:F0} deg";
+                });
+            speedSlider
+                .onValueChanged
+                .AddListener(value => {
+                    speedText.text = $"{value:F0} deg/frame";
+                });
+        }
+    }
+}
